828345286772c029d98f84c4365e3d3d24c42c4c
isabelrios <isabelrios@aurigae.com>
2015-07-30 12:58:36 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,22 @@
 var models = require('../models/models.js');
 
+//GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( //crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+		res.render('quizes/new', {quiz:quiz});
+};
+
+//POST /quizes/create
+exports.create = function (req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+//guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	}) //Redirección HTTP (URL relativo) lista de preguntas
+};
 
 //GET /author
 exports.author = function(req, res){
--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-//router.get('/quizes/question', quizController.question);
-//router.get('/quizes/answer', quizController.answer);
-//router.get('/quizes/search', quizController.search);
 
 //Modificar, definición de rutas de /quizes
+
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -19,5 +17,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', quizController.author);
 
 router.get('/quizes/search', quizController.search);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 module.exports = router;
