93b88dc71f6a026b40c9a94bbe1681dd47e7814c
isabelrios <isabelrios@aurigae.com>
2015-08-03 19:19:57 +0200

    e autor!

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 //router.get('/author', quizController.author);
 router.get('/author', function (req, res) {
-    res.render('author.ejs', { title: 'Creditos', errors: []});
+    res.render('author', { title: 'Creditos', errors: []});
 });
 
 router.get('/quizes/search', quizController.search);
