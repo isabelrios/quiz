a73109e4023faddb31f5c489945bb381e6df39f1
isabelrios <isabelrios@aurigae.com>
2015-08-06 10:05:01 +0200

    autenticacion y sesion

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -8,17 +8,18 @@ exports.new = function (req, res) {
 //POST /quizes/:quizId/comments
 exports.create = function (req, res) {
 	var comment = models.Comment.build(
-		{texto:req.body.comment.texto,
-			QuizId:req.params.quizId
+		{texto: req.body.comment.texto,
+			QuizId: req.params.quizId
 		});
+
 	comment
 	.validate()
 	.then(function (err){
 		if(err){
-		res.render('comments/new.ejs',{comment:comment, errors:err.errors});
+		res.render('comments/new.ejs',{comment:comment, quizid: req.params.quizId, errors:err.errors});
 	}else{
 		comment
 		.save()
-		.then(function (){res.redirect('/quizes/'+req.params.quizId)})
+		.then( function (){ res.redirect('/quizes/'+req.params.quizId)})
 	}}).catch(function (error){next(error)});
 };
