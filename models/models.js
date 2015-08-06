f9117807f9140307a45b6e9d078cd457a0263417
isabelrios <isabelrios@aurigae.com>
2015-08-06 09:08:50 +0200

    comentario

--- models/models.js
+++ models/models.js
@@ -29,8 +29,16 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+//Importar la definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 //Exporta la definicion de la tabla Quiz 
 exports.Quiz = Quiz; 
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicailiza tabla de preguntas en DB
 sequelize.sync().then(function(){
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+
+var commentController = require('../controllers/comment_controller');
+
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index.ejs', { title: 'Quiz', errors: []});
@@ -25,6 +28,9 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
